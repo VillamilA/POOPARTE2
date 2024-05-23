fb7852aa4e3e48d115e61b0dce85868c030ce210
POO <angel.villamil@epn.edu.ec>
2024-05-23 09:08:46 -0500

    PARTE 2 actualizada

--- src/PRESETNACION/taller.java
+++ src/PRESETNACION/taller.java
@@ -1,5 +1,5 @@
 package PRESETNACION;
-
+//esto agrego
 public class taller {
     String nombre;
     int edad;
@@ -8,6 +8,33 @@ public taller (String nombre, int edad){
     this.edad = edad;
 }
 public void Presentacion(){
-    System.out.println("Soy el estudiante "+nombre+" y tengo "+edad+" años");
+    System.out.println("Soy  "+nombre+" estudiante de la esfot y tengo "+edad);
 }
+}
+
+class Padre extends Familia{
+    String nombrePapa;
+    public Padre (String apellido, String origen, String nombrePapa){
+        super(apellido,origen);
+        this.nombrePapa= nombrePapa;
+    }
+    public void apellidopapa(){
+        System.out.println("El señor" +nombrePapa+" tiene el apellido "+apellido);
+
+    }
+}
+
+class Hija extends Padre{
+    String nombrehija;
+    int edad;
+
+    public Hija(String apellido, String origen, String nombrePapa, String nombrehija, int edad){
+        super(apellido,origen, nombrePapa);
+        this.nombrehija = nombrehija;
+        this.edad = edad;
+    }
+
+    public void infoHija(){
+        System.out.println("la hija "+nombrehija + "hija del señor "+ nombrePapa+" tiene el apellido "+ apellido);
+    }
 }
